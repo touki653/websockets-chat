849deeb0cd299a05523d05a781e0056312ee69d1
touki653 <g.vincendon@vithemis.com>
2014-05-21 09:05:44 +0200

    [DependencyInjection] Implemented CompilerPass for commands

---
+++ src/DependencyInjection/CompilerPass/ChatCommandCompilerPass.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Touki\ChatBundle\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Chat commands compiler pass understands chat.command service tags
+ *
+ * @author Touki <g.vincendon@vithemis.com>
+ */
+class ChatCommandCompilerPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $holder = $container->getDefinition('chat.command_resolver');
+
+        foreach ($container->findTaggedServiceIds('chat.command') as $service => $attributes) {
+            $holder->addMethodCall('add', [new Reference($service)]);
+        }
+    }
+}
--- src/ToukiChatBundle.php
+++ src/ToukiChatBundle.php
@@ -3,10 +3,21 @@
 namespace Touki\ChatBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Touki\ChatBundle\DependencyInjection\CompilerPass\ChatCommandCompilerPass;
 
 /**
  * Bundle declaration for ToukiChat
  */
 class ToukiChatBundle extends Bundle
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ChatCommandCompilerPass);
+    }
 }
